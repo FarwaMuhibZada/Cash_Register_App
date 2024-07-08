f5e14d89923998f09e92691ccb63fd6b6b51108c
farwaMuhibzada <farwafarid2017@gmail.com>
2024-07-08 12:42:10 +0430

    added script file and target all element of html file.

---
+++ script.js
@@ -0,0 +1,4 @@
+let cash = document.getElementById("cash");
+let displayChangeDue = document.getElementById("change-due");
+let purchaseBtn = document.getElementById("purchase-btn");
+let displayCid = document.getElementById("cash-in-drawer");
