501178d2912029795112367fac54286209138165
farwaMuhibzada <farwafarid2017@gmail.com>
2024-07-08 15:44:18 +0430

    fixed linters error .

--- script.js
+++ script.js
@@ -31,56 +31,56 @@ const checkRegister = () => {
   if (cashInt < price) {
     alert('Customer does not have enough money to purchase the item');
     return;
+  } else if (cashInt === price) {
+    displayChangeDue.innerText = 'No change due - customer paid with exact cash';
+    return;
+  } else if (cashInput.value === '') {
+    return;
   }
 
-  if (cashInt === price) {
-    displayChangeDue.innerText = 'No change due - customer paid with exact cash';
-  } else if (cashInput.value !== '') {
-    if (change > totalCid) {
-      displayChangeDue.innerText = 'Status: INSUFFICIENT_FUNDS';
-    } else {
-      const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-      const denominationNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
-      const changeArr = [];
-      const cidCopy = [...cid];
+  if (change > totalCid) {
+    displayChangeDue.innerText = 'Status: INSUFFICIENT_FUNDS';
+    return;
+  }
 
-      for (let i = 0; i < denominations.length; i += 1) {
-        let totalDenom = 0;
-        while (change >= denominations[i] && cidCopy[cidCopy.length - 1 - i][1] > 0) {
-          cidCopy[cidCopy.length - 1 - i][1] = Number(
-            (cidCopy[cidCopy.length - 1 - i][1] - denominations[i]).toFixed(2)
-          );
-          change = Number((change - denominations[i]).toFixed(2));
-          totalDenom += denominations[i];
-        }
-        if (totalDenom > 0) {
-          changeArr.push([denominationNames[i], totalDenom]);
-        }
-      }
+  const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  const denominationNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+  const changeArr = [];
+  const cidCopy = [...cid];
 
-      if (change > 0) {
-        displayChangeDue.innerText = 'Status: INSUFFICIENT_FUNDS';
-      } else {
-        const remainingCid = cidCopy.reduce((total, cash) => total + cash[1], 0);
-
-        if (remainingCid === 0) {
-          displayChangeDue.innerHTML = `Status: CLOSED ${changeArr.map((cash) => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)}`).join(' ')}`;
-          cid = cid.map((denom) => [denom[0], 0]);
-        } else {
-          displayChangeDue.innerHTML = `Status: <b>OPEN</b> <br><br>${changeArr.map((cash) => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`).join(' ')}`;
-          cid = cidCopy;
-        }
-      }
+  for (let i = 0; i < denominations.length; i += 1) {
+    let totalDenom = 0;
+    while (change >= denominations[i] && cidCopy[cidCopy.length - 1 - i][1] > 0) {
+      cidCopy[cidCopy.length - 1 - i][1] = Number(
+        (cidCopy[cidCopy.length - 1 - i][1] - denominations[i]).toFixed(2)
+      );
+      change = Number((change - denominations[i]).toFixed(2));
+      totalDenom += denominations[i];
+    }
+    if (totalDenom > 0) {
+      changeArr.push([denominationNames[i], totalDenom]);
     }
   }
 
+  if (change > 0) {
+    displayChangeDue.innerText = 'Status: INSUFFICIENT_FUNDS';
+    return;
+  }
+
+  const remainingCid = cidCopy.reduce((total, cash) => total + cash[1], 0);
+
+  if (remainingCid === 0) {
+    displayChangeDue.innerHTML = `Status: CLOSED ${changeArr.map((cash) => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)}`).join(' ')}`;
+    cid = cid.map((denom) => [denom[0], 0]);
+  } else {
+    displayChangeDue.innerHTML = `Status: <b>OPEN</b> <br><br>${changeArr.map((cash) => `<b>${cash[0]}</b>: $${cash[1].toFixed(2)} <br>`).join(' ')}`;
+    cid = cidCopy;
+  };
+
   displayCashInDrawer();
 };
-
-
 window.onload = displayCashInDrawer;
 purchaseBtn.addEventListener('click', checkRegister);
-
 cashInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     checkRegister();
